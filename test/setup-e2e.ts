1138d8d4fb1dac7d7c1768d09dedae9bfd378c02
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-30 11:48:16 -0300

    feat(test): add setup-e2e.ts file for end-to-end testing setup
    This commit adds a new file `setup-e2e.ts` to the `test` directory. The file contains setup code for end-to-end testing. Here's a summary of the changes:
    
    - Import necessary dependencies: `PrismaClient` from `@prisma/client`, `config` from `dotenv`, `execSync` from `node:child_process`, and `randomUUID` from `node:crypto`.
    - Configure environment variables: Load `.env` and `.env.test` files using `config` function.
    - Create an instance of `PrismaClient`.
    - Implement `generateUniqueDatabaseURL` function: This function takes a `schemaId` as input and generates a unique database URL by appending the `schemaId` as a query parameter to the `DATABASE_URL` environment variable.
    - Generate a random `schemaId` using `randomUUID`.
    - Implement `beforeAll` hook: This hook is executed before all tests. It generates a unique database URL using `generateUniqueDatabaseURL` and sets it as the value of `DATABASE_URL` environment variable. It then deploys the Prisma migrations using `pnpm prisma migrate deploy` command.
    - Implement `afterAll` hook: This hook is executed after all tests. It drops the schema with the generated `schemaId` using `prisma.$executeRawUnsafe` and disconnects the `PrismaClient` instance.
    
    These changes are made to improve the setup for end-to-end testing and ensure that each test run has its own isolated database schema.

---
+++ test/setup-e2e.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+import { config } from 'dotenv'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+config({ path: '.env', override: true })
+config({ path: '.env.test', override: true })
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
