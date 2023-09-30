13ab0dd9ab15eca4189302f817678e9e63541354
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-30 12:52:53 -0300

    feat(audio.presenter.ts): add status field to the HTTP response of the AudioPresenter to provide information about the audio status

--- src/infra/http/presenters/audio.presenter.ts
+++ src/infra/http/presenters/audio.presenter.ts
@@ -4,6 +4,7 @@ export class AudioPresenter {
   static toHTTP(audio: AudioEntity) {
     return {
       id: audio.id.toString(),
+      status: audio.status,
       title: audio.name,
       srt: audio.srt,
       transcription: audio.transcription,
