be9a0515d431fd1e28ca529b7d5114d45751c99b
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2023-09-30 12:53:44 -0300

    fix(main.ts): create temp folder if it doesn't exist to prevent errors when using it feat(main.ts): enable CORS to allow cross-origin requests

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core'
+import { existsSync, mkdirSync } from 'node:fs'
+import { resolve } from 'node:path'
 
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
 async function bootstrap() {
+  const pathTemp = resolve(__dirname, '../temp')
+
+  const existFolderTemp = existsSync(pathTemp)
+
+  if (!existFolderTemp) {
+    mkdirSync(pathTemp)
+  }
+
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
 
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
